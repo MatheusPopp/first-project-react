ca079667ceaeece5b58fcc8cf173d96f5bd66432
Matheus Furtado de Souza Popp <matheus.popp@pca.com.br>
2019-07-26 13:27:58 -0300

    Implementação inclusão e listagem de dados (Consulta à api localhost)

--- src/App.js
+++ src/App.js
@@ -2,25 +2,77 @@ import React, { Component } from 'react';
 import './App.css';
 import './css/pure-min.css'
 import './css/side-menu.css'
+import $ from 'jquery'
 
 class App extends Component {
 
+  //Construtor da classe (componente)
   constructor() {
     //necessário chamar o construtor da classe pai para acessar o 'this'
     super();
     //estado do objeto -> state única varíavel que o react observa
     this.state = {
-      lista: [
-        { nome: 'Matheus', email: 'matheus.popp@outlook.com', senha: 123456 },
-        { nome: 'Matheus', email: 'matheus.popp@outlook.com', senha: 123456 },
-        { nome: 'Matheus', email: 'matheus.popp@outlook.com', senha: 123456 },
-        { nome: 'Matheus', email: 'matheus.popp@outlook.com', senha: 123456 }
-      ]
+      dataSource: [],
+      nome: '',
+      email: '',
+      senha: ''
+
     };
+
+    //Associa o this da classe para o 'enviaForm' -> dentro do 'enviaForm' ele não reconhece o 'this' automaticamente, assim é necessário realizar o bind
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
+  }
+
+  setNome(evento) {
+    evento.preventDefault();
+    this.setState({ nome: evento.target.value });
+  }
+
+  setEmail(evento) {
+    evento.preventDefault();
+    this.setState({ email: evento.target.value });
+  }
+
+  setSenha(evento) {
+    evento.preventDefault();
+    this.setState({ senha: evento.target.value });
   }
 
 
+  //Método descontinuado, utilizar componentDidMount para ações que produzem side-effects ou o construtor para casos que não produzem side-effects
+  componentDidMount() {
+    $.ajax({
+      url: "http://localhost:8080/api/autores",
+      dataType: 'json',
+      success: function (result) {
+        //Atualização do estado do componente -> quando utilizado o 'setState' o React renderiza o componente novamente
+        this.setState({ dataSource: result });
+      }.bind(this) //bind o 'this' do react -> informa que o 'this' é do react e não do jquery
+    });
+  }
 
+  enviaForm(evento) {
+    evento.preventDefault();
+    $.ajax({
+      url: "http://localhost:8080/api/autores",
+      contentType: 'application/json',
+      dataType: 'json',
+      type: 'post',
+      data: JSON.stringify({ nome: this.state.nome, email: this.state.email, senha: this.state.senha }),
+      success: function (result) {
+        this.setState({ dataSource: result });
+        console.log("enviado com sucesso");
+      }.bind(this),
+      error: function (restul) {
+        console.log("erro");
+      }
+    });
+  }
+
+  //Método para renderizar o html (Componente)
   render() {
     return (
       <div id="layout">
@@ -46,18 +98,18 @@ class App extends Component {
           </div>
           <div className="content" id="content">
             <div className="pure-form pure-form-aligned">
-              <form className="pure-form pure-form-aligned">
+              <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="POST">
                 <div className="pure-control-group">
                   <label htmlFor="nome">Nome</label>
-                  <input id="nome" type="text" name="nome" value="" />
+                  <input required id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />
                 </div>
                 <div className="pure-control-group">
                   <label htmlFor="email">Email</label>
-                  <input id="email" type="email" name="email" value="" />
+                  <input required id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />
                 </div>
                 <div className="pure-control-group">
                   <label htmlFor="senha">Senha</label>
-                  <input id="senha" type="password" name="senha" />
+                  <input required id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />
                 </div>
                 <div className="pure-control-group">
                   <label></label>
@@ -72,17 +124,15 @@ class App extends Component {
                   <tr>
                     <th>Nome</th>
                     <th>Email</th>
-                    <th>Senha</th>
                   </tr>
                 </thead>
                 <tbody>
                   {
-                    this.state.lista.map(x => {
+                    this.state.dataSource.map(x => {
                       return (
-                        <tr>
+                        <tr key={x.id}>
                           <td>{x.nome}</td>
                           <td>{x.email}</td>
-                          <td>{x.senha}</td>
                         </tr>
                       );
                     })
