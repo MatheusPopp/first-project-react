9196c3420345dbcfe70d2c80fbc29324bf122283
Matheus Furtado de Souza Popp <matheus.popp@pca.com.br>
2019-07-26 13:44:52 -0300

    Implelementação de novo componente - 'InputCustomizado'

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import './css/pure-min.css'
 import './css/side-menu.css'
 import $ from 'jquery'
+import InputCustomizado from './componentes/InputCustomizado'
 
 class App extends Component {
 
@@ -99,18 +100,9 @@ class App extends Component {
           <div className="content" id="content">
             <div className="pure-form pure-form-aligned">
               <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="POST">
-                <div className="pure-control-group">
-                  <label htmlFor="nome">Nome</label>
-                  <input required id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />
-                </div>
-                <div className="pure-control-group">
-                  <label htmlFor="email">Email</label>
-                  <input required id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />
-                </div>
-                <div className="pure-control-group">
-                  <label htmlFor="senha">Senha</label>
-                  <input required id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />
-                </div>
+                <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome" htmlFor="nome"/>
+                <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email" htmlFor="email"/>
+                <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha" htmlFor="senha"/>
                 <div className="pure-control-group">
                   <label></label>
                   <button type="submit" className="pure-button pure-button-primary">Gravar</button>
